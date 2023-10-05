b06646d9d1a18ea0fb59ec139b1e6a34dcff8d14
jahnsam <jahn.samuel@gmail.com>
2023-10-05 12:36:29 -0700

    folder clean-up

--- analyze_methyls.m
+++ analyze_methyls.m
@@ -1,7 +1,9 @@
 function analyze_methyls(Nuclei,file_name)
 
 fileID = fopen(file_name,'w');
-header = 'methyl_group,center_x,center_y,center_z,normal_x,normal_y,normal_z\n';
+header = ['methyl_group,','center_x,center_y,center_z,',...
+    'normal_x,normal_y,normal_z,',...
+    'plane_x,plane_y,plane_z\n'];
 fprintf(fileID,header);
 
 methyl_ids = unique(Nuclei.MethylID);
@@ -25,11 +27,20 @@ for id = methyl_ids
   center = mean(r_hydrogens,1);
   center_str = sprintf('%d,',center);
 
-  normal = center - r_carbon;
-  normal = normal/norm(normal);
-  normal_str = sprintf('%d,',normal);
+  axis = center - r_carbon;
+  axis = axis/norm(axis);
+
+  plane_dir = r_hydrogens(1,:) - center;
+  plane_dir = plane_dir/norm(plane_dir);
+  plane_dir_str = sprintf('%d,',plane_dir);
  
-  line = [methyl_name,',',center_str,normal_str];
+  normal = cross(r_hydrogens(2,:) - center,plane_dir);
+  normal = normal/norm(normal);
+  if normal'*axis < 0
+    normal = -normal;
+  end
+  normal_str = sprintf('%d,',normal);
+  line = [methyl_name,',',center_str,normal_str,plane_dir_str];
   line = [line(1:end-1),'\n'];
   
   fprintf(fileID,line);
